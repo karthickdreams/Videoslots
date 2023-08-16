212741f6bcc558279dd09fd49268015a79dbe93b
Karthickmonishrajsoundararajan <128609899+karthickdreams@users.noreply.github.com>
2023-08-16 17:25:16 +0200

    Update BaseClass.java

--- Evernote Tests/Evernote login/BaseClass.java
+++ Evernote Tests/Evernote login/BaseClass.java
@@ -1,25 +1,39 @@
 package com.R;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class BaseClass {
-
+public class BaseClass
+{
 	public WebDriver driver;
 
-	public static void sendingTheInputValue(WebElement element, String inputvalue)
+	// Description: Function used to set a value in a text box in the application
+	// Parameters:
+	// Input: element - [object] - Webelement from the webdriver
+	// Input : inputValue [String] - value to be set in the text box
 
+	public static void setInputText(WebElement element, String inputValue)
 	{
-		element.sendKeys(inputvalue);
+		element.sendKeys(inputValue);
 	}
 
-	public static void clickWebElement(WebElement clickElement) {
+	// Description: Function used to Click a web element in the application
+	// Parameters:
+	// Input: element - [object] - Webelement to be clicked 
+	
+	public static void clickWebElement(WebElement clickElement) 
+	{
 		clickElement.click();
 	}
 
-	public static String getFromWebPage(WebElement getTextElement) {
-		String textfromWebPage = getTextElement.getText();
-		return textfromWebPage;
+	// Description: Function used to get a text value from the application
+	// Parameters:
+	// Input: getTextElement - [object] - Webelement from where the text to be fetched
+	// Output: strTextFromWebElement - [String] - value fetched from the web element
+	
+	public static String getFromWebPage(WebElement getTextElement) 
+	{
+		String strTextFromWebElement = getTextElement.getText();
+		
+		return strTextFromWebElement;
 	}
-
 }
