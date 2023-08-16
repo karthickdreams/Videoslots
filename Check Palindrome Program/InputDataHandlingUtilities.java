ae1e5639f0daa14191b0730e422df03ee42cc6e2
Karthickmonishrajsoundararajan <128609899+karthickdreams@users.noreply.github.com>
2023-08-16 17:11:18 +0200

    Update InputDataHandlingUtilities.java

--- Check Palindrome Program/InputDataHandlingUtilities.java
+++ Check Palindrome Program/InputDataHandlingUtilities.java
@@ -1,5 +1,4 @@
 package Demo;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -13,8 +12,8 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 
-public class InputDataHandlingUtilities {
-
+public class InputDataHandlingUtilities 
+{
 	List<Object> listOfInput = new ArrayList<Object>();
 
 	// Description: Function to read input data from an excel file in a given "sheet"
@@ -23,8 +22,8 @@ public class InputDataHandlingUtilities {
 	// Input : strSheetName - [String] - Specify the sheet name from where the data must be read
 	// Output : List - [List] - returns the list of values from the column "A"
 
-	public List inputFromExcel(String strInputExcelPath, String strSheetName) throws IOException {
-
+	public List inputFromExcel(String strInputExcelPath, String strSheetName) throws IOException 
+	{
 		//Initiate excel object and sheet object
 		File inputExcelFileObject = new File(strInputExcelPath);
 		FileInputStream ExcelInputStreamObject = new FileInputStream(inputExcelFileObject);
@@ -34,15 +33,14 @@ public class InputDataHandlingUtilities {
 		int intRowCount = strSheetObject.getPhysicalNumberOfRows();
 
 		// Traverse row by row and add the values present in the excel into the list
-		for (int inputRowNumber = 1; inputRowNumber < intRowCount; inputRowNumber++) {
-
+		for (int inputRowNumber = 1; inputRowNumber < intRowCount; inputRowNumber++) 
+		{
 			Row currentIterationRowInputString = strSheetObject.getRow(inputRowNumber);
 			Cell firstCellString = currentIterationRowInputString.getCell(0);
 			listOfInput.add(firstCellString);
 		}
-
+		
 		return listOfInput;
-
 	}
 
 	// Description: Function to write boolean data into an excel file in a given "sheet"
@@ -52,8 +50,8 @@ public class InputDataHandlingUtilities {
 	// Input : boolOutputValue - [boolean] - Either True or false from the place of calling
 	// Input : intOutputRownumber - [Integer] - Row number where the output to be written
 
-	public void exportToExcel(String strInputExcelPath, String strSheetName, boolean boolOutputValue, int intOutputRownumber) throws IOException {
-
+	public void exportToExcel(String strInputExcelPath, String strSheetName, boolean boolOutputValue, int intOutputRownumber) throws IOException 
+	{
 		File outputExcelFileObject = new File(strInputExcelPath);
 		FileInputStream excelInputStreamObject = new FileInputStream(outputExcelFileObject);
 		Workbook workbookObject = new XSSFWorkbook(excelInputStreamObject);
@@ -64,7 +62,6 @@ public class InputDataHandlingUtilities {
 		OutputResult.createCell(1).setCellValue(boolOutputValue);
 		FileOutputStream excelOutputStreamObject = new FileOutputStream(outputExcelFileObject);
 		workbookObject.write(excelOutputStreamObject);
-
 	}
 
 	// Description: Function to write Integer data into an excel file in a given "sheet"
@@ -74,8 +71,8 @@ public class InputDataHandlingUtilities {
 	// Input : intResult - [Integer] - Result of the opertion to be written on the excel
 	// Input : intOutputRownumber - [Integer] - Row number where the output to be written
 
-	public void resultToExcel(String strInputExcelPath, String strSheetName, int intResult, int intOutputRownumber) throws IOException {
-
+	public void resultToExcel(String strInputExcelPath, String strSheetName, int intResult, int intOutputRownumber) throws IOException 
+	{
 		File outputExcelFileObject = new File(strInputExcelPath);
 		FileInputStream excelInputStreamObject = new FileInputStream(outputExcelFileObject);
 		Workbook workbookObject = new XSSFWorkbook(excelInputStreamObject);
@@ -86,7 +83,5 @@ public class InputDataHandlingUtilities {
 
 		FileOutputStream excelOutputStreamObject = new FileOutputStream(outputExcelFileObject);
 		workbookObject.write(excelOutputStreamObject);
-
 	}
-
 }
