15c61f2a564b3935550d6bec38904e2080d05251
Karthickmonishrajsoundararajan <128609899+karthickdreams@users.noreply.github.com>
2023-08-17 19:32:26 +0530

    Update and rename StepDefNoteCheckv1.java to StepDefNoteCheck.java

--- Task5/Everynote Create Note/StepDefNoteCheck.java
+++ Task5/Everynote Create Note/StepDefNoteCheck.java
@@ -1,20 +1,18 @@
 package StepDef;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import com.R.BaseClass;
-
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class StepDefNoteCheck extends BaseClass {
+public class StepDefNoteCheck extends BaseClass 
+{
 	public WebDriver driver;
 
 	@Before
@@ -32,51 +30,42 @@ public class StepDefNoteCheck extends BaseClass {
 
 	@When("user able to login")
 	public void user_able_to_login() {
-
 		WebElement objLoginButton = FindWebElement("\"//button[@id=\\\"qa-CREATE_NOTE\\\"]\"");
 		clickWebElement(objLoginButton);
-
 	}
 
 	@When("user can click new note")
 	public void user_can_click_new_note() {
-
 		WebElement objEvernoteNewNoteField = FindWebElement("//span[text()='Note']");
 		clickWebElement(objEvernoteNewNoteField);
-
 	}
 
 	@When("user can enter new note")
 	public void user_can_enter_note() {
-
 		WebElement objTitleText = FindWebElement("//textarea[@placeholder=\"Title\"])");
 		String actualInputTile = "Welcome to testing";
-		sendingTheInputValue(objTitleText, actualInputTile);
+		setInputText(objTitleText, actualInputTile);
 		WebElement objInputNoteField = FindWebElement("//en-note[@id=\"en-note\"]");
 		String actualInputNote = "Types of testing";
-		sendingTheInputValue(objInputNoteField, actualInputNote);
-
+		setInputText(objInputNoteField, actualInputNote);
 	}
 
 	@Then("user can able to see the logout message")
 	public void user_can_able_to_see_the_logout_message() {
 		WebElement objUserNameField = FindWebElement("//div[@class=\"mjp8WyYQODySClV2byHt\"]");
 		clickWebElement(objUserNameField);
-	
 		WebElement objDropDownLogout = FindWebElement("//a[@id=\"qa-ACCOUNT_DROPDOWN_LOGOUT\"]");
 		clickWebElement(objDropDownLogout);
+		
 		String expectedlogoutmessage = "You have logged out of Evernote.";
-		String actuallogoutmessage = driver.findElement(By.xpath("//h1[text()=\"You have logged out of Evernote.\"]"))
-				.getText();
+		String actuallogoutmessage = driver.findElement(By.xpath("//h1[text()=\"You have logged out of Evernote.\"]")).getText();
 		assertEquals(actuallogoutmessage, expectedlogoutmessage);
-
 	}
 
 	@When("user can able to login")
 	public void user_can_able_to_login() {
 		WebElement objLoginButton = FindWebElement("\"//button[@id=\\\"qa-CREATE_NOTE\\\"]\"");
 		clickWebElement(objLoginButton);
-
 	}
 
 	@When("user can click the recent note")
@@ -91,11 +80,10 @@ public class StepDefNoteCheck extends BaseClass {
 		WebElement objActualTitleMessage = FindWebElement("//textarea[@placeholder=\"Title\"]");
 		assertEquals(objActualTitleMessage, expectedTileMessage);
 	}
+	
 	public WebElement FindWebElement(String strxPathIdentifier)
 	{
-				WebElement returnWebElement = driver.findElement(By.xpath(strxPathIdentifier));
-				return returnWebElement;
-
+		WebElement returnWebElement = driver.findElement(By.xpath(strxPathIdentifier));
+		return returnWebElement;
 	}
-
 }
