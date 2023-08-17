cf195cc2f5d9de4510d83c86417f8426cadfa7f6
Karthickmonishrajsoundararajan <128609899+karthickdreams@users.noreply.github.com>
2023-08-17 19:17:30 +0530

    Add files via upload

---
+++ Task5/Everynote Create Note/RunnerNoteCheck.java
@@ -0,0 +1,20 @@
+package Runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.core.snippets.SnippetType;
+import io.cucumber.java.Before;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "C:\\Users\\karth\\eclipse-workspace\\neweclipse\\R\\src\\test\\java\\com\\R\\checkData.feature",
+glue = "StepDef", 
+dryRun = !true,
+monochrome = true,
+plugin = "pretty"
+)
+
+public class Runner {
+
+}
